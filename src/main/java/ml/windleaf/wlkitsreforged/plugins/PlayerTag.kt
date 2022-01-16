3fc09e388de39f6e3b64f11ce5cff0687d44060b
WindLeaf233 <mc.windleaf@foxmail.com>
2022-01-16 13:24:27 +0800

    fix: unused 'enabled'

--- src/main/java/ml/windleaf/wlkitsreforged/plugins/PlayerTag.kt
+++ src/main/java/ml/windleaf/wlkitsreforged/plugins/PlayerTag.kt
@@ -30,82 +30,84 @@ class PlayerTag : Plugin, Listener, CommandExecutor, TabCompleter {
     }
 
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<String?>): Boolean {
-        if (args.isEmpty()) Util.invalidArgs(sender) else {
-            val i = HashMap<String, String>()
-            i["playerName"] = name
-            when (args[0]) {
-                "help" -> {
-                    val helps = HashMap<String, String>()
-                    helps["/playertag help"] = "查看此帮助"
-                    helps["/playertag set [player] [tag]"] = "设置一个玩家的称号"
-                    helps["/playertag remove [player]"] = "删除一个玩家的称号"
-                    helps["/playertag check [player]"] = "查看一个玩家的称号"
-                    helps["/playertag reset [player]"] = "重置一个玩家的称号/名称"
-                    Util.sendHelp(sender, helps)
-                }
-                "set" -> {
-                    if (args.size < 3) Util.invalidArgs(sender)
-                    else {
-                        val n = args[1]!!
-                        val tag = args[2]!!
-                        val player = Bukkit.getPlayer(n)
-                        if (player == null) noPlayer(sender, i)
-                        else {
-                            i["tag"] = Util.translateColorCode(tag)!!
-                            playerTags[Util.getUUID(player)] = Util.insert(Util.getPluginConfig(name, "format") as String, i)!!
-                            player.setDisplayName(Util.translateColorCode(tag)!!)
-                            FileUtil.saveHashMap(playerTags, path)
-                            Util.send(sender, Util.insert(Util.getPluginMsg(name, "set-success"), i))
-                        }
+        if (enabled) {
+            if (args.isEmpty()) Util.invalidArgs(sender) else {
+                val i = HashMap<String, String>()
+                i["playerName"] = name
+                when (args[0]) {
+                    "help" -> {
+                        val helps = HashMap<String, String>()
+                        helps["/playertag help"] = "查看此帮助"
+                        helps["/playertag set [player] [tag]"] = "设置一个玩家的称号"
+                        helps["/playertag remove [player]"] = "删除一个玩家的称号"
+                        helps["/playertag check [player]"] = "查看一个玩家的称号"
+                        helps["/playertag reset [player]"] = "重置一个玩家的称号/名称"
+                        Util.sendHelp(sender, helps)
                     }
-                }
-                "remove" -> {
-                    if (args.size < 2) Util.invalidArgs(sender)
-                    else {
-                        val n = args[1]!!
-                        val player = Bukkit.getPlayer(n)
-                        if (player == null) noPlayer(sender, i)
+                    "set" -> {
+                        if (args.size < 3) Util.invalidArgs(sender)
                         else {
-                            playerTags.remove(Util.getUUID(player))
-                            FileUtil.saveHashMap(playerTags, path)
-                            Util.send(sender, Util.insert(Util.getPluginMsg(name, "del-success"), i))
-                        }
-                    }
-                }
-                "check" -> {
-                    if (args.size < 2) Util.invalidArgs(sender)
-                    else {
-                        val n = args[1]!!
-                        val player = Bukkit.getPlayer(n)
-                        if (player == null) noPlayer(sender, i)
-                        else {
-                            val tag = playerTags[Util.getUUID(player)]
-                            i["tag"] = tag!!
-                            when (tag) {
-                                null -> noTag(sender, i)
-                                "" -> noTag(sender, i)
-                                else -> Util.send(sender, Util.insert(Util.getPluginMsg(name, "get"), i))
+                            val n = args[1]!!
+                            val tag = args[2]!!
+                            val player = Bukkit.getPlayer(n)
+                            if (player == null) noPlayer(sender, i)
+                            else {
+                                i["tag"] = Util.translateColorCode(tag)!!
+                                playerTags[Util.getUUID(player)] = Util.insert(Util.getPluginConfig(name, "format") as String, i)!!
+                                player.setDisplayName(Util.translateColorCode(tag)!!)
+                                FileUtil.saveHashMap(playerTags, path)
+                                Util.send(sender, Util.insert(Util.getPluginMsg(name, "set-success"), i))
                             }
                         }
                     }
-                }
-                "reset" -> {
-                    if (args.size < 2) Util.invalidArgs(sender)
-                    else {
-                        val n = args[1]!!
-                        val player = Bukkit.getPlayer(n)
-                        if (player == null) noPlayer(sender, i)
+                    "remove" -> {
+                        if (args.size < 2) Util.invalidArgs(sender)
                         else {
-                            playerTags.remove(Util.getUUID(player))
-                            player.setDisplayName(player.name)
-                            FileUtil.saveHashMap(playerTags, path)
-                            Util.send(sender, Util.insert(Util.getPluginMsg(name, "reset-success"), i))
+                            val n = args[1]!!
+                            val player = Bukkit.getPlayer(n)
+                            if (player == null) noPlayer(sender, i)
+                            else {
+                                playerTags.remove(Util.getUUID(player))
+                                FileUtil.saveHashMap(playerTags, path)
+                                Util.send(sender, Util.insert(Util.getPluginMsg(name, "del-success"), i))
+                            }
                         }
                     }
+                    "check" -> {
+                        if (args.size < 2) Util.invalidArgs(sender)
+                        else {
+                            val n = args[1]!!
+                            val player = Bukkit.getPlayer(n)
+                            if (player == null) noPlayer(sender, i)
+                            else {
+                                val tag = playerTags[Util.getUUID(player)]
+                                i["tag"] = tag!!
+                                when (tag) {
+                                    null -> noTag(sender, i)
+                                    "" -> noTag(sender, i)
+                                    else -> Util.send(sender, Util.insert(Util.getPluginMsg(name, "get"), i))
+                                }
+                            }
+                        }
+                    }
+                    "reset" -> {
+                        if (args.size < 2) Util.invalidArgs(sender)
+                        else {
+                            val n = args[1]!!
+                            val player = Bukkit.getPlayer(n)
+                            if (player == null) noPlayer(sender, i)
+                            else {
+                                playerTags.remove(Util.getUUID(player))
+                                player.setDisplayName(player.name)
+                                FileUtil.saveHashMap(playerTags, path)
+                                Util.send(sender, Util.insert(Util.getPluginMsg(name, "reset-success"), i))
+                            }
+                        }
+                    }
+                    else -> Util.invalidArgs(sender)
                 }
-                else -> Util.invalidArgs(sender)
             }
-        }
+        } else Util.disabled(sender)
         return false
     }
 
