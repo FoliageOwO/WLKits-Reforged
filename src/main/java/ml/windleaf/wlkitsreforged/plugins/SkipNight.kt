110769be75f1ff2644276351905bc215b1da042b
Nzzz964 <2582787306@qq.com>
2022-01-25 00:12:35 +0800

    fix: SkipNight known bugs

--- src/main/java/ml/windleaf/wlkitsreforged/plugins/SkipNight.kt
+++ src/main/java/ml/windleaf/wlkitsreforged/plugins/SkipNight.kt
@@ -6,7 +6,6 @@ import ml.windleaf.wlkitsreforged.core.Plugin
 import ml.windleaf.wlkitsreforged.core.WLKits
 import ml.windleaf.wlkitsreforged.utils.Util
 import org.bukkit.Bukkit
-import org.bukkit.World
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
@@ -28,54 +27,61 @@ class SkipNight : Plugin, Listener {
 
     @EventHandler
     fun onPlayerBedEnterEvent(e: PlayerBedEnterEvent) {
-        if (enabled
-            && (e.player.world.time >= 12010 || e.player.world.isThundering)
-            && Util.hasPermission(e.player, "skipnight", PermissionType.ACTION)
-        ) {
-            val percent = Util.getPluginConfig(name, "percent") as Int
-            if (percent < 0 || percent > 100) {
-                WLKits.log("&c错误: 配置文件中 &6plugins.skipnight.percent &c数值小于 0 或大于 100, 请重新进行配置!")
-                return
-            }
+        if (!enabled) return
+        if (!Util.hasPermission(e.player, "skipnight", PermissionType.ACTION)) return
+        if (e.bedEnterResult !== PlayerBedEnterEvent.BedEnterResult.OK) return
 
-            if (!onBed.contains(e.player)) onBed.add(e.player)
-
-            val online = Bukkit.getOnlinePlayers().size
-
-            val onBedPercent = onBed.size / online
-
-            if (onBedPercent >= (percent / 100)) {
-                Util.broadcastPlayers(Util.getPluginMsg(name, "msg-ok"))
-                // 设置世界时间
-                e.player.world.time = 100
-                return
-            }
-
-            val offBed = online - onBed.size
-            // assume another N player is entered bed
-            var need = 1
-
-            while (need < offBed) {
-                val expect = (onBed.size + need) / online
-                if (expect >= (percent / 100)) {
-                    break
-                }
-                need++
-            }
-
-            Util.broadcastPlayers(
-                Util.insert(
-                    Util.getPluginMsg(name, "msg-need"),
-                    "onBed" to onBed.size.toString(),
-                    "needPlayers" to need.toString()
-                )
-            )
-
+        val percent = Util.getPluginConfig(name, "percent") as Int
+        // 百分比小数形式
+        val percentDecimals = percent.toDouble() / 100
+        if (percent < 0 || percent > 100) {
+            WLKits.log("&c错误: 配置文件中 &6plugins.skipnight.percent &c数值小于 0 或大于 100, 请重新进行配置!")
+            return
         }
+
+        if (!onBed.contains(e.player)) onBed.add(e.player)
+
+        val online = Bukkit.getOnlinePlayers().size
+
+        val onBedPercent = onBed.size.toDouble() / online.toDouble()
+        if (onBedPercent >= percentDecimals) {
+            Util.broadcastPlayers(Util.getPluginMsg(name, "msg-ok"))
+            // 判断天气
+            if (e.player.world.isThundering) {
+                // 设置为晴天
+                e.player.world.setStorm(false)
+                e.player.world.isThundering = false
+            }
+            // 设置世界时间
+            e.player.world.time = 0
+            return
+        }
+
+        val offBed = online - onBed.size
+        // assume another N player is entered bed
+        var need = 1
+        while (need < offBed) {
+            val expect = (onBed.size + need).toDouble() / online.toDouble()
+            if (expect >= percentDecimals) {
+                break
+            }
+            need++
+        }
+
+        Util.broadcastPlayers(
+            Util.insert(
+                Util.getPluginMsg(name, "msg-need"),
+                "onBed" to onBed.size.toString(),
+                "needPlayers" to need.toString()
+            )
+        )
+
     }
 
     @EventHandler
     fun onPlayerBedLeaveEvent(e: PlayerBedLeaveEvent) {
-        if (enabled && Util.hasPermission(e.player, "skipnight", PermissionType.ACTION)) onBed.remove(e.player)
+        if (!enabled) return
+        if (!Util.hasPermission(e.player, "skipnight", PermissionType.ACTION)) return
+        onBed.remove(e.player)
     }
 }
