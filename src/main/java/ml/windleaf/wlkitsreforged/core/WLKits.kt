652db8351cb3f16832936ba6d8c6820d872ca5f3
WindLeaf233 <mc.windleaf@foxmail.com>
2022-01-30 19:40:05 +0800

    kdoc: WLKits main class

--- src/main/java/ml/windleaf/wlkitsreforged/core/WLKits.kt
+++ src/main/java/ml/windleaf/wlkitsreforged/core/WLKits.kt
@@ -4,37 +4,94 @@ import com.google.gson.Gson
 import ml.windleaf.wlkitsreforged.core.module.ModuleManager
 import ml.windleaf.wlkitsreforged.core.module.commanding.Commanding
 import ml.windleaf.wlkitsreforged.core.reflect.Reflector
+import ml.windleaf.wlkitsreforged.data.Data
 import ml.windleaf.wlkitsreforged.internal.YamlData
 import ml.windleaf.wlkitsreforged.utils.FileUtil
 import ml.windleaf.wlkitsreforged.utils.Util
 import org.bukkit.plugin.java.JavaPlugin
 import java.util.stream.Collectors
 
+/**
+ * The WLKits main class
+ */
 class WLKits : JavaPlugin() {
     companion object {
+        /**
+         * The name of WLKits
+         */
         const val name = "WLKits"
+
+        /**
+         * The version of WLKits
+         */
         const val version = "0.2.2"
+
+        /**
+         * The instance of WLKits main class, will be initialized in [onEnable]
+         */
         lateinit var instance: WLKits
+
+        /**
+         * The instance of [Reflector], get by [Util.getReflector]
+         *
+         * @see Reflector
+         */
         lateinit var reflector: Reflector
+
+        /**
+         * The instance of [YamlData], saving `message.yml` in resources folder
+         */
         lateinit var message: YamlData
+
+        /**
+         * The boolean of whether the WLKits is running in debug mode,
+         * in the debug mode, the plugin will print out all the debug information
+         */
         var debug = false
+
+        /**
+         * The instance of [Gson], used to convert json to object and parse json
+         *
+         * @see Gson
+         * @see Data
+         */
         val gson = Gson()
+
+        /**
+         * The instance of [Commanding], which used to process commands and tab completers
+         */
         val commanding = Commanding()
 
+        /**
+         * Prints out the message without [Reflector]
+         */
         fun log(any: Any) = log(null, any)
 
+        /**
+         * Prints out the message with [Reflector]
+         */
         private fun log(ref: Reflector? = null, any: Any) {
             val r = ref ?: reflector
             r.sendConsole("${Util.withPrefix()}$any")
         }
 
+        /**
+         * Prints out the debug message without [Reflector]
+         */
         fun debug(vararg any: Any?) = debug(null, *any)
+
+        /**
+         * Prints out the debug message with [Reflector]
+         */
         fun debug(ref: Reflector?, vararg any: Any?) {
             val str = any.asList().stream().map { it.toString() }.collect(Collectors.joining(" "))
             val prefix = "&c&l[DEBUG]&r "
             if (debug) log(ref, "$prefix$str")
         }
 
+        /**
+         * Reloads WLKits, reloads commands, reload modules, reload all configs
+         */
         fun reload() {
             instance.reloadConfig()
             message.loadDataFromFile()
